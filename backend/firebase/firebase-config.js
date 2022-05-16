4ccd5cc06925f450e53735f33f8fb42d0973dfa5
Puujee04 <purevsuren04@gmail.com>
2022-05-16 13:55:29 +0800

    email password avah

---
+++ backend/firebase/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-74ZhuPx-V-qxFUeaCJhNzm7REVMVQfk",
+  authDomain: "website-team1.firebaseapp.com",
+  projectId: "website-team1",
+  storageBucket: "website-team1.appspot.com",
+  messagingSenderId: "1017416791665",
+  appId: "1:1017416791665:web:af5cd02d6bda3c86f4cdd0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
--- backend/main.js
+++ backend/main.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const router = express.Router();
 const app = express();
-const port = 3000;
 
+// App configure
+dotenv.config({ path: "./config/config.env" });
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({
+    data: [
+      {
+        product1: "1",
+        product2: "2",
+        product3: "3",
+      },
+    ],
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.post("/", (req, res) => {
+  console.log(req.body.email);
+  res.send({ success: true });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
