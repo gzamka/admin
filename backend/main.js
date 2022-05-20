05d5d2db5fe29de178b05bc4f7056c8cae466179
javha <jjavkhlan123@gmail.com>
2022-05-20 12:25:10 +0800

    done

--- backend/main.js
+++ backend/main.js
@@ -1,15 +1,22 @@
-import express from "express";
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import products from './crud/products.js'
+import news from './crud/routes.js'
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
 import router from "./routes/routes.js";
-
-const app = express();
-
-// App configure
+const app = express()
 dotenv.config({ path: "./config/config.env" });
-app.use(express.json());
-app.use(bodyParser.json());
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+app.use(express.json())
+app.use(cors())
+app.use('/news', news)
 app.use("/", router);
+
+app.use('/products', products)
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
-});
+    console.log(`Example app listening on port ${process.env.PORT}`);
+  });
+  
