959ec86ff2a4971adcb83c754ba10e5d725fdf0d
gzamka <g.zamka1@gmail.com>
2022-06-06 11:37:16 +0800

    conflict

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Choose, Footer } from './components'
 
 export const App = () => {
@@ -14,11 +13,3 @@ export const App = () => {
     </div>
   );
 }
-=======
-
-function App() {
-  return 
-}
-
-export default App;
->>>>>>> e8c1b2403efdc7893c06ef3cbad2dabf9abe6129
--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import {App} from './App';
-=======
-import App from './App';
->>>>>>> e8c1b2403efdc7893c06ef3cbad2dabf9abe6129
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +8,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-
-=======
->>>>>>> e8c1b2403efdc7893c06ef3cbad2dabf9abe6129
