172277c2387f6606c32773eafec9f5cf83802569
gzamka <g.zamka1@gmail.com>
2022-06-24 10:36:46 +0800

    g

--- admin/src/firebase/firebase.js
+++ admin/src/firebase/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID
+  apiKey: "AIzaSyC50Vb9vCVqdiQT5yAePzN2d1Yp3OdefKM",
+  authDomain: "demoday-3c8d0.firebaseapp.com",
+  projectId: "demoday-3c8d0",
+  storageBucket: "demoday-3c8d0.appspot.com",
+  messagingSenderId: "712774865518",
+  appId: "1:712774865518:web:cae8a81ade42dd3c24cc7f"
 };
 export const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(firebaseApp);
